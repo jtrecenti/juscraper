1389d01b54898b0c053a8c29022e694db12dfc9b
Bruno da C. de Oliveira <bruno.oliveira.pgsp@gmail.com>
2025-02-26 19:50:24 -0300

    Utilizei o raspador do TJSP como base para criar um raspador que funciona na plataforma https://comunica.pje.jus.br/
    Por enquanto, raspa apenas as 5 primeiras páginas.

--- src/juscraper/tribunal_manager.py
+++ src/juscraper/tribunal_manager.py
@@ -1,5 +1,6 @@
 from .tjsp_scraper import TJSP_Scraper
 from .tjrs_scraper import TJRS_Scraper
+from .comunicaCNJ_scraper import comunicaCNJ_Scraper
 
 def scraper(tribunal_name: str, **kwargs):
     """Retorna o raspador correspondente ao tribunal solicitado."""
@@ -9,5 +10,7 @@ def scraper(tribunal_name: str, **kwargs):
         return TJSP_Scraper(**kwargs)
     elif tribunal_name == "TJRS":
         return TJRS_Scraper()
+    elif tribunal_name == 'COMUNICA_CNJ':
+        return comunicaCNJ_Scraper(**kwargs)
     else:
         raise ValueError(f"Tribunal '{tribunal_name}' ainda não é suportado.")
