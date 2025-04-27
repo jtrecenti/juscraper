9ea81b79d95550bf135573d2cacb3f9f4de68de6
Julio Trecenti <julio.trecenti@gmail.com>
2025-04-27 19:59:58 -0300

    add jusbr scraper

--- src/juscraper/tribunal_manager.py
+++ src/juscraper/tribunal_manager.py
@@ -1,5 +1,7 @@
 from .tjsp_scraper import TJSP_Scraper
 from .tjrs_scraper import TJRS_Scraper
+from .tjpr_scraper import TJPR_Scraper
+from .jusbr_scraper import JUSBR_Scraper
 
 def scraper(tribunal_name: str, **kwargs):
     """Retorna o raspador correspondente ao tribunal solicitado."""
@@ -9,5 +11,9 @@ def scraper(tribunal_name: str, **kwargs):
         return TJSP_Scraper(**kwargs)
     elif tribunal_name == "TJRS":
         return TJRS_Scraper()
+    elif tribunal_name == "TJPR":
+        return TJPR_Scraper()
+    elif tribunal_name == "JUSBR":
+        return JUSBR_Scraper(**kwargs)
     else:
         raise ValueError(f"Tribunal '{tribunal_name}' ainda não é suportado.")
